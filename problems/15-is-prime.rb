4fd039419b24f766c0db9250be8a2de2ed4a189f
wathika <vcwathika@gmail.com>
2015-08-21 22:30:14 +0300

    solved prime number

--- problems/15-is-prime.rb
+++ problems/15-is-prime.rb
@@ -13,6 +13,18 @@
 # Difficulty: medium.
 
 def is_prime?(number)
+  if number < 1
+    return false
+  end
+
+  x = 2
+  while x < number
+    if (number % x) == 0
+      return false
+    end
+     x += 1
+  end
+  return true
 end
 
 # These are tests to check that your code is working. After writing
