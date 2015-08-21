194138855345751e836bd39eca7c0549bcf18667
wathika <vcwathika@gmail.com>
2015-08-21 22:48:08 +0300

    completed successfully with loop./. theres a better method though

--- problems/07-most-letters.rb
+++ problems/07-most-letters.rb
@@ -6,6 +6,26 @@
 
 def nearby_az(string)
 	idx1 = 0
+
+while idx1 < string.length
+    if string[idx1] != "a"
+      idx1 += 1
+      next
+    end
+
+    idx2 = idx1 + 1
+    while (idx2 < string.length) && (idx2 <= idx1 + 3)
+      if string[idx2] == "z"
+        return true
+      end
+
+      idx2 += 1
+    end
+
+    idx1 += 1
+end
+
+  return false
 end
 
 # These are tests to check that your code is working. After writing
